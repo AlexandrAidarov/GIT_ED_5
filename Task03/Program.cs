a4e5c553982b2ae6d3597fdee0fb74723d6338ce
Alexandr Aidarov <aidarovi7ia@mail.ru>
2022-12-18 16:47:03 +0300

    Task03 done

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите номер дня недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine($"{number} - Понедельник");
+}
+else if (number == 2)
+{
+    Console.WriteLine($"{number} - Вторник");
+}
+else if (number == 3)
+{
+    Console.WriteLine($"{number} - Среда");
+}
+else if (number == 4)
+{
+    Console.WriteLine($"{number} - Четверг");
+}
+else if (number == 5)
+{
+    Console.WriteLine($"{number} - Пятница");
+}
+else if (number == 6)
+{
+    Console.WriteLine($"{number} - Суббота");
+}
+else if (number == 7)
+{
+    Console.WriteLine($"{number} - Воскресенье");
+}
+else 
+{
+    Console.WriteLine($"{number} - не корректное число");
+}
