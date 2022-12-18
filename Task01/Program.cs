674cfb78d11f4f8315bc13d309862ff118d86871
Alexandr Aidarov <aidarovi7ia@mail.ru>
2022-12-18 16:31:26 +0300

    Task01 done

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите первое целое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.WriteLine($"Да, {number1} является квадратом числа {number2}");
+}
+else
+ {
+     Console.WriteLine($"Нет, {number1} не является квадратом числа {number2}");
+ }
