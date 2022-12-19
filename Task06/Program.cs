f66bbb651732c2d324f79ee26b224a5c7a89606f
Alexandr Aidarov <aidarovi7ia@mail.ru>
2022-12-19 20:57:00 +0300

    Task06 done

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое целое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число:");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (max < number2 && number2 > number3)
+{
+    max = number2;
+    Console.WriteLine($"Max - {max}");
+}
+else if (max < number3 && number3 > number2)
+{
+    max = number3;
+    Console.WriteLine($"Max - {max}");
+}
+else
+{
+    Console.WriteLine($"Max - {max}");
+}
