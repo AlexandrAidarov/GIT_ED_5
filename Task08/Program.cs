9d604ce6cf548177669f1b32bf4baadad03195e5
Alexandr Aidarov <aidarovi7ia@mail.ru>
2022-12-19 21:15:38 +0300

    Task08, and Homework 5-1 done

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int n = 2;
+while (number >= n)
+{
+    Console.Write(n + " ");
+    n = n + 2;
+}
