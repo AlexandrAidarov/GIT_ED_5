93b0dbf2cd230ee7e41bb33716baac7cf8991a3a
Alexandr Aidarov <aidarovi7ia@mail.ru>
2022-12-24 14:06:58 +0300

    Task09 done

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10-99 => {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра => {firstDigit} ");
+else Console.WriteLine($"Наибольшая цифра => {secondDigit} ");
+
