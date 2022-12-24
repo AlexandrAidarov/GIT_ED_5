c4f79beb4decd4b9bd545e68a11f2dfec0d17912
Alexandr Aidarov <aidarovi7ia@mail.ru>
2022-12-24 15:17:12 +0300

    Task11 and Task12 done

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число => {number}");
+
+int firdNumber = number % 10;
+int firstNumber = number / 100;
+int result = (firstNumber * 10) + firdNumber;
+
+Console.WriteLine(result);
--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber % secondNumber == 0)
+{
+    Console.WriteLine("Кратное.");
+}
+else
+{
+     Console.WriteLine($"Не кратное, остаток - {firstNumber % secondNumber}");
+}
