960d756a8b81f405dbf87a7692386c69782af9cb
Alexandr Aidarov <aidarovi7ia@mail.ru>
2022-12-24 15:33:14 +0300

    Task14 done

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// кратно ли число 7 и 23
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 23 == 0 && number % 7 == 0)
+{
+    Console.WriteLine("Кратное.");
+}
+else
+{
+     Console.WriteLine("Не кратное");
+}
