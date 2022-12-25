d85db69924b41130932202a333c1f57b51ab4868
Alexandr Aidarov <aidarovi7ia@mail.ru>
2022-12-25 17:24:37 +0300

    Task20,22 done

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите координаты первой точки:");
+Console.Write("X: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Y: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки:");
+Console.Write("X: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Y: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double distance = Math.Round(Distance(x1, y1, x2, y2), 2, MidpointRounding.ToZero);
+
+Console.WriteLine(distance);
+
+double Distance(double xa, double ya, double xb, double yb)
+{
+    double result = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2));
+    return result;
+}
+
--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableSquare(number);
+
+void TableSquare(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count}     " + Math.Pow(count, 2));
+        Console.WriteLine();
+        count++;
+    }
+
+}
