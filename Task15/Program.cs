d4f19454111fa71cbc750c235a02559dbb0d8ee3
Alexandr Aidarov <aidarovi7ia@mail.ru>
2022-12-25 11:54:43 +0300

    Task15 done

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -4,3 +4,17 @@
 // 7 -> да
 // 1 -> нет
 
+Console.WriteLine("Введите номер дня недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 7) Console.WriteLine("Такого дня недели нет!");
+else
+{
+    Console.WriteLine(SquareNumber(number) ? "да" : "нет");
+}
+
+bool SquareNumber(int num1)
+{
+    return num1 % 6 == 0 || num1 % 7 == 0;
+}
+
