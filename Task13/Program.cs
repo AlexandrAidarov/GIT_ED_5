19bc023d4bb947a10f6c8f53fbcb62dd6b45782d
Alexandr Aidarov <aidarovi7ia@mail.ru>
2022-12-25 11:42:17 +0300

    Task13 done

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет. Выполнить с помощью числовых операций 
+//(целочисленное деление, остаток от деления).
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number < 0) number = -number;
+
+if (number > 99)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+
+    int firdNumber = number % 10;
+    Console.WriteLine($"Третья цифра => {firdNumber}");
+}
+else if (number < -99)
+{
+    while (number < -999)
+    {
+        number = number / 10;
+    }
+
+    int firdNumber = number % 10 * -1;
+    Console.WriteLine($"Третья цифра => {firdNumber}");
+}
+else
+{
+    Console.WriteLine("Третьего числа нет.");
+}
+
