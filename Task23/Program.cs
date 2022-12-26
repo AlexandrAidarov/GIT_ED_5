f1ddba3ad9e1529a261db682a5c84052ebbf6222
Alexandr Aidarov <aidarovi7ia@mail.ru>
2022-12-26 19:45:58 +0300

    Task23 done

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableSquare(number);
+
+void TableSquare(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count, 3} {Math.Pow(count, 3),6}");
+        count++;
+    }
+}
+
