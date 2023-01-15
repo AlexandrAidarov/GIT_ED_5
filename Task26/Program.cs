5839db6d4d37aea43aa44f850d7784147b13be37
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-01-15 12:30:12 +0300

    Task26 done

---
+++ Task26/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №26: принимает число, выдает количество цифр в числе.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int numbers = Numbers(number);
+Console.WriteLine($"В числе {number} : {numbers} цифр");
+
+int Numbers(int num)
+{
+    int count = 1;
+    while (num >= 10)
+    {
+        num = num / 10;
+        count = count + 1;
+    }
+    return count;
+}
+
