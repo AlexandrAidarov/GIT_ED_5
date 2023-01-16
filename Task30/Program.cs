dc817425eabc515fa1186570881b80bb71d85b2b
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-01-16 21:00:19 +0300

    Task30 done

---
+++ Task30/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задание №30
+// Напишите программу которая выводит массив из 8 чисел и заполняет его 0 и 1.
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0,2);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]+ " ");
+    }
+}
