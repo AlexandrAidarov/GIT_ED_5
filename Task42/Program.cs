a8fbcd590e9a97aceb8896f4b4c9a0efffb6deae
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-01-24 21:10:22 +0300

    Task42 done

---
+++ Task42/Program.cs
@@ -0,0 +1,15 @@
+﻿// 42 число пееписать в двоичный код
+
+string res = ConvertDecToBin(2);
+Console.WriteLine(res);
+
+string ConvertDecToBin(int num)
+{
+    string result = string.Empty;
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num = num / 2;
+    }
+    return result;
+}
