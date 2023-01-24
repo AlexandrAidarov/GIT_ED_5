0b701cc9c60b8a6f29f32df05b273e75a0c9d27a
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-01-24 20:55:47 +0300

    Task39 done

---
+++ Task40/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsExistTriangle(number1, number2, number3);
+Console.WriteLine(result ? "Такой треугольник существует" : "Такого треугольника не существует");
+
+bool IsExistTriangle(int num1, int num2, int num3)
+{
+    int sum1 = num1 + num2;
+    int sum2 = num1 + num3;
+    int sum3 = num2 + num3;
+    return sum1 > num3 && sum2 > num2  && sum3 > num1;
+}
