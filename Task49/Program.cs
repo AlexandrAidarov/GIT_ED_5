10c65efa39a51bd69b78cce912c37fb07fc4bc8c
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-01-29 14:05:41 +0300

    Task52 done

---
+++ Task49/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 49
+// Создаем  двумерный массив, возводим в квадрат если оба индекса четные 
+
+int[,] CreateMatrixRndInt(int rows, int coluums, int min, int max)
+{
+    int[,] matrix = new int[rows, coluums];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 5}, ");
+            else Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, -99, 99);
+PrintMatrix(array2D);
+
--- Task52/Program.cs
+++ Task52/Program.cs
@@ -7,14 +7,14 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-int[,] CreateArrayRndInt(int rows, int coluums, int min, int max)
+int[,] CreateMatrixRndInt(int rows, int coluums, int min, int max)
 {
     int[,] matrix = new int[rows, coluums];
     Random rnd = new Random();
 
-    for (int i = 0; i < matrix.GetLengthength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLengthength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
             matrix[i, j] = rnd.Next(min, max + 1);
         }
@@ -22,3 +22,43 @@ int[,] CreateArrayRndInt(int rows, int coluums, int min, int max)
     return matrix;
 }
 
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void FindAnswer(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write("[");
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+
+            if (i < matrix.GetLength(0) - 1) sum = matrix[i, j] + sum;
+            else
+            {
+                sum = matrix[i, j] + sum;
+                double average = Math.Round(sum / matrix.GetLength(0), 2, MidpointRounding.ToZero);
+                Console.WriteLine(average);
+            }
+
+        }
+
+    }
+}
+
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+FindAnswer(array2D);
