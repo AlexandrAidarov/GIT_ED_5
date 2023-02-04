3adbe338b24f9b6e68a1e6b232d74eee02370445
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-02-04 14:20:23 +0300

    Task65 done

---
+++ Task65/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 58: Задайте значения M и N. Напишите программу которая выведет
+все натуральные числа от M до N
+
+*/
+
+Console.WriteLine("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+NumbersRange(numberOne, numberTwo);
+
+void NumbersRange(int numOne, int numTwo)
+{
+    if (numOne == numTwo) Console.Write($"{numOne} ");
+    else if (numOne < numTwo)
+    {
+        Console.Write($"{numOne} ");
+        NumbersRange(numOne + 1, numTwo);
+    }
+    else
+    {
+        Console.Write($"{numOne} ");
+        NumbersRange(numOne - 1, numTwo);
+    }
+   
+}
